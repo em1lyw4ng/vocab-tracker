44f1bdb3be825b55eaf5bc25c4a44d63b84e08a4
zxz002 <emily.wang024@gmail.com>
2022-03-09 00:19:10 -0800

    Initial Commit

---
+++ Vocab Tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Vocab Tracker
+//
+//  Created by Emily on 3/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Vocab Tracker/Vocab_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vocab_TrackerApp.swift
+//  Vocab Tracker
+//
+//  Created by Emily on 3/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Vocab_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
