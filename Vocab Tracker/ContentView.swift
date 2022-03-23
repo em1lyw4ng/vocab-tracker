4e7abf8a661e8348c209a08546d592255f25384e
zxz002 <emily.wang024@gmail.com>
2022-03-22 21:26:33 -0700

    navbar

--- Vocab Tracker/ContentView.swift
+++ Vocab Tracker/ContentView.swift
@@ -7,10 +7,89 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ZStack {
+                Color.red
+            }
+            .navigationTitle("Home")
+        }
+    }
+}
+
+
+struct ContentView: View {
+    @State var  selectedIndex = 0
+    
+    let icons = ["house", "plus", "chart.bar.xaxis"]
+    var body: some View {
+        VStack {
+            
+            //Content
+            ZStack {
+                switch selectedIndex {
+                case 0:
+                    NavigationView {
+                        VStack {
+                            Text("First")
+                        }.navigationTitle("Home")
+                    }
+                    
+                case 1:
+                    NavigationView {
+                        VStack {
+                            Text("Second")
+                        }.navigationTitle("Create")
+                    }
+                    
+                default:
+                    NavigationView {
+                        VStack {
+                            Text("Third")
+                        }.navigationTitle("Stats")
+                    }
+                    
+                }
+
+            }
+            
+            Spacer()
+            
+            Divider()
+            HStack {
+                ForEach(0..<3, id : \.self) {
+                    number in
+                    Spacer()
+                    Button(action: {self.selectedIndex = number},
+                           label : {
+                        if number == 1 {
+                            Image(systemName: icons[number])
+                                .font(.system(size:25, weight: .regular, design: .default))
+                                .foregroundColor(.white)
+                                .frame(width: 40, height: 40)
+                                .background(Color.blue)
+                                .cornerRadius(30)
+                        }
+                        else {
+                            Image(systemName: icons[number])
+                                .font(.system(size:25, weight: .regular, design: .default))
+                                .foregroundColor(selectedIndex == number ? .black : Color(UIColor.lightGray))
+                            
+                        }
+                    })
+                      
+                    if number != 2 {
+                        Spacer()
+                    }
+                    
+                    Spacer()
+                    
+                }
+                
+            }
+        }
+
     }
 }
 
